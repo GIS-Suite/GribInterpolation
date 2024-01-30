ab014f071053c49f885ad011f27083cda8379bdc
Derrick Poon <zhinkee@gmail.com>
2024-01-30 12:18:29 -0600

    testing run configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
         System.out.println("Hello world!");
+        System.out.println(args[1]);
     }
 }
