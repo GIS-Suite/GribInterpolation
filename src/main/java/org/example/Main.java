592657b639e0eddfbbfd980615cfb173a5ea380f
Derrick Poon <zhinkee@gmail.com>
2024-01-31 18:11:37 -0600

    run-config reads all grb files in folder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,26 @@
 package org.example;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        try{
+            File directoryPath = new File(args[0]);
+            String[] grbFilePaths = directoryPath.list();
+            System.out.println(directoryPath);
 
-        for (String arg : args) {
-            System.out.println(arg);
-
-            String grib1_path = args[0];
-            //String grib2_path = "src/main/java/test/data/NAVGEM__2023081518__global_1440x721__air_temp__flight_lvl__10000000__00000000__fcst_ops__					0060.grb2";
-            try{
-                var testGrib1 = ucar.nc2.dt.grid.GridDataset.open(grib1_path);
-                //var testGrib2 = ucar.nc2.dt.grid.GridDataset.open(grib2_path);
+        for (int i = 0; i < grbFilePaths.length; i++) {
+            System.out.println(grbFilePaths[i]);
+       }
+            for (int i = 0; i < grbFilePaths.length; ++i) {
+                System.out.println(grbFilePaths);
+                String gribFile = grbFilePaths[i];
+                var testGrib1 = ucar.nc2.dt.grid.GridDataset.open(directoryPath + "\\" + gribFile);
                 System.out.println(testGrib1.getDataVariables() + "\n");
-                //System.out.println(testGrib2.getDataVariables());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
+        }catch (IOException e) {
+            throw new RuntimeException(e);
         }
         System.out.println("Hello world!");
         
