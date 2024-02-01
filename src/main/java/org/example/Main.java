ef7d34da8ce4d4e92e582798aa185c41822114ce
Derrick Poon <zhinkee@gmail.com>
2024-02-01 10:49:19 -0600

    Run configuration: folder path as program argument
    Passing folder path of grib files into main as program argument now working.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,33 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try{
-            File directoryPath = new File(args[0]);
-            String[] grbFilePaths = directoryPath.list();
-            System.out.println(directoryPath);
+        //check if args is empty
+        if (args.length == 0){
+            System.out.println("The arguments array is empty. Please pass at least 1 argument into main.");
+            System.exit(0);
+        }
 
-        for (int i = 0; i < grbFilePaths.length; i++) {
-            System.out.println(grbFilePaths[i]);
-       }
-            for (int i = 0; i < grbFilePaths.length; ++i) {
-                System.out.println(grbFilePaths);
-                String gribFile = grbFilePaths[i];
-                var testGrib1 = ucar.nc2.dt.grid.GridDataset.open(directoryPath + "\\" + gribFile);
-                System.out.println(testGrib1.getDataVariables() + "\n");
+        //Creating String array of grib file names from folder.
+        try{
+            File folderPath = new File(args[0]);
+            String[] gribFileNames = folderPath.list();
+
+            if (gribFileNames != null && gribFileNames.length != 0) {
+                //Combining folder path with grib file name to create full grib file path to read all grib files
+                for (int i = 0; i < gribFileNames.length; ++i) {
+                    System.out.println(gribFileNames[i]);
+                    String gribFileName = gribFileNames[i];
+                    String gribFilePath = folderPath + "\\" + gribFileName;
+                    var testGrib1 = ucar.nc2.dt.grid.GridDataset.open(gribFilePath);
+                    System.out.println(testGrib1.getDataVariables() + "\n");
+                }
+            }else {
+                System.out.println("The folder is empty. Please check folder.");
+                System.exit(0);
             }
         }catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Hello world!");
-        
+
     }
 }
