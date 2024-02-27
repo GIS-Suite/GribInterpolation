4d1dcc8fcee2dcb6d202707cf916daff6e0d2b34
Anna <anna.valdez@selu.edu>
2024-02-27 00:12:57 -0600

    WIP
    added test values

--- src/test/java/com/github/gissuite/gribinterpolation/NearestNeighborTest.java
+++ src/test/java/com/github/gissuite/gribinterpolation/NearestNeighborTest.java
@@ -9,12 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class NearestNeighborTest {
     @Test
     public void distanceTest(){
-        //anna pls give test data here
-        double latitude1 = 1500;
-        double longitude1 = 1600;
-        double latitude2 = 1300;
-        double longitude2 = 600;
-        double expectedResult = 1500;
+        //test values converted to radians, actual long and lat in comments
+        double latitude1 = -1.151917306; //-66.0
+        double longitude1 = 1.084896663; //62.16
+        double latitude2 = -1.117010721; //-64.0
+        double longitude2 = 1.08768919; //62.32
+        double expectedResult = 223.0605246;
+
+        //extra example
+        double latitude3 = -1.151917306; //-66.0
+        double longitude3 = 1.084896663; //62.16
+        double latitude4 = -1.151917306; //-66.0
+        double longitude4 = 1.08768919; //62.32
+        double expectedResult2 = 17.79118952;
 
         double result = NearestNeighbor.haverSine(latitude1,longitude1,latitude2,longitude2);
         assertEquals(expectedResult, result);
