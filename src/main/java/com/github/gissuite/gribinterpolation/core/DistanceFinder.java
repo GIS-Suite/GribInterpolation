d800817b9b87b45926b4f89ad940a1ee25f05b6c
Alyssa Major <alyssamajor2020@gmail.com>
2024-02-28 20:38:09 -0600

    reformatted and added function for to convert to radians

--- src/main/java/com/github/gissuite/gribinterpolation/core/DistanceFinder.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/DistanceFinder.java
@@ -6,25 +6,19 @@ public class DistanceFinder {
 
     }
     public static double haverSine(double latitude1, double longitude1, double latitude2, double longitude2) {
-        double r = 6371.0; //radians
-        // If data is in degrees change all the latitude and longitudes to radians with the following:
-
+        double r = 6371.0;
          latitude1 = convertToRadians(latitude1);
          longitude1= convertToRadians(longitude1);
          latitude2 = convertToRadians(latitude2);
          longitude2 = convertToRadians(longitude2);
 
-        double aTest  =latitude2-latitude1;
-        double aTest2 = longitude2-longitude1;
         double sineSquaredOfLat = Math.pow((Math.sin((latitude2 - latitude1) / 2)), 2);
         double sineSquaredOfLong = Math.pow((Math.sin((longitude2 - longitude1) / 2)), 2);
-        //Quadratic Formula
 
-        double A = sineSquaredOfLat + Math.cos((latitude1)) * (Math.cos((latitude2)) * sineSquaredOfLong);
-//        double B= (2 *r)* Math.asin(Math.sqrt(sineSquaredOfLat + Math.cos((latitude1)) * (Math.cos((latitude2)) * sineSquaredOfLong)));
-        double C = 2 * Math.atan2(Math.sqrt(A), Math.sqrt(1 - A));
-        double distance = r * C;
-//        double b = r * (A);
+        double A = sineSquaredOfLat + Math.cos(latitude1) * (Math.cos(latitude2) * sineSquaredOfLong);
+
+        double distance =(2 * r) * Math.asin(Math.sqrt(A));
+
         return distance;
     }
 
--- src/test/java/com/github/gissuite/gribinterpolation/DistanceFinderTest.java
+++ src/test/java/com/github/gissuite/gribinterpolation/DistanceFinderTest.java
@@ -14,14 +14,14 @@ public class DistanceFinderTest {
         double longitude1 = 1.084896663; //62.16
         double latitude2 = -1.117010721; //-64.0
         double longitude2 = 1.08768919; //62.32
-        double expectedResult = 222.5163299;
+        double expectedResult = 3.8938310894808135;
 
         //extra example
         double latitude3 = -1.151917306; //-66.0
         double longitude3 = 1.084896663; //62.16
         double latitude4 = -1.151917306; //-66.0
         double longitude4 = 1.08768919; //62.32
-        double expectedResult2 = 7.237163323;
+        double expectedResult2 = 0.31045208183444206;
 
         double result = DistanceFinder.haverSine(latitude1,longitude1,latitude2,longitude2);
 //        double result = DistanceFinder.haverSine(latitude3,longitude3,latitude4,longitude4);
