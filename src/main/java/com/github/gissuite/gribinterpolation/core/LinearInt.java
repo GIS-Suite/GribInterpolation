fb48cf49df6258ba95cc2ec5feb8d05b8888d666
Anna <anna.valdez@selu.edu>
2024-03-15 16:57:26 -0500

    Array of data points
    now puts all of the interpolated data points into an array

--- src/main/java/com/github/gissuite/gribinterpolation/core/LinearInt.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/LinearInt.java
@@ -4,11 +4,24 @@ import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
 
 public class LinearInt {
     public static void Interpolate(double[] datapoint1, double[] datapoint2){
-        LinearInterpolator what = new LinearInterpolator();
+        LinearInterpolator equation = new LinearInterpolator();
         double[] depth = {datapoint1[3], datapoint2[3]};
         double[] temp = {datapoint1[2], datapoint2[2]};
+        double[][] dataPointArray = new double[(int)(datapoint2[3]-datapoint1[3]-1)][4];
+        int j = 0;
         for(double i=depth[0]+1; i<depth[1]; i++){
-            System.out.println("Temp at depth "+ i + ": " + what.interpolate(depth, temp).value(i));
+            dataPointArray[j][0] = datapoint1[0]; //latitude
+            dataPointArray[j][1] = datapoint1[1]; //longitude
+            dataPointArray[j][2] = equation.interpolate(depth, temp).value(i);
+            dataPointArray[j][3] = i;
+            j++;
+        }
+
+        for(int l = 0; l<dataPointArray.length; l++){
+            for(int x=0; x<4; x++) {
+                System.out.print(dataPointArray[l][x] + " ");
+            }
+            System.out.println();
         }
     }
 }
