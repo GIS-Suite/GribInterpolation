11c50ac1e56185365b4964837362ab531a170336
Alyssa Major <alyssamajor2020@gmail.com>
2024-03-16 20:49:00 -0500

    wip need actual long lat values

--- src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
@@ -5,26 +5,33 @@ public class NearestNeighbor {
         //datapoints[0] = {lat,long,temp,depth}
         double[] point1 = {3,3,37,-4};
         double[] point2 = {3,4, 43, -4};
-        double[] point3 = {6,8, 54, -3};
-        double[] point4 = {10, 9, 73, 5};
-        double[] point5 = {2,3, 90, 3};
+        double[] point3 = {2,3, 38, -4};
+        double[] point4 = {3, 4,44, -3};
+        double[] point5 = {2,3, 90, -3};
         double[][] points = {point1,point2,point3,point4,point5};
         double[] pointinterpolating= {2,3,-4}; // only lat, long, depth //want to find temp
-        getNearestNeighbor(points, pointinterpolating);
+        getNearestNeighbor(points, pointinterpolating, 2);
     }
     public static double[][] getNearestNeighbor(double[][] points, double[] Pointinterpolating, int k) {
         double[][] nearestNeighbors = new double[k][4];
         double[] distancesFromPointInterpolating = new double[k];
-        int counter = 0;
         double maxValue = Double.MAX_VALUE;
-        for (int i = 0; i < points.length; i++) {
-            double closetPoint = DistanceFinder.haverSine(points[i][0], points[0][i], Pointinterpolating[0],Pointinterpolating[1]);
-            if (closetPoint < maxValue){
-                maxValue = closetPoint;
-                nearestNeighbors[i][] = closetPoint;
-            }
-            else if (dist1 > dist2) {
 
+        for (int i = 0; i < points.length; i++) {
+            double distance = DistanceFinder.haverSine(points[i][0], points[0][i], Pointinterpolating[0],Pointinterpolating[1]);
+            if (distance < maxValue){
+                maxValue = distance;
+//                nearestNeighbors[i][] = closetPoint; //function to get lat/long/depth/temp and put in array of nearestNeighbors
+
+                distancesFromPointInterpolating[i] = distance;
+                if (distance < distancesFromPointInterpolating[i]){
+                    distancesFromPointInterpolating[i] = distance;
+                            //maybe a sort to sort all the points in acending order based on distances????
+                }
+
+            }
+            else if (distance > maxValue) {
+                //do nothing
             }
             }
 
