4e713e7ebbdde501b8b4b8433bb135c60ef108f9
Devin Ford <devin.ford@selu.edu>
2024-03-19 13:42:49 -0500

    better format

--- src/main/java/com/github/gissuite/gribinterpolation/core/BicubicInterpolation.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/BicubicInterpolation.java
@@ -4,51 +4,57 @@ import org.apache.commons.math3.analysis.interpolation.BicubicInterpolator;
 import com.github.gissuite.gribinterpolation.data.DataPoint;
 
 public class BicubicInterpolation {
-    BicubicInterpolator bicubic = new BicubicInterpolator();
-    DataPoint[][] points = new DataPoint[4][4];
-
-    BicubicInterpolation(){
-        //creating fake data points
-        float lat = 60;
-        float longi = 60;
-        float tempk = 300;
-
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                points[i][j] = new DataPoint(lat, longi, tempk);
-                lat += 0.5;
-                tempk += 5;
-            }
-            longi += 0.5;
-        }
+    private BicubicInterpolatingFunction bicubicfunc;
+    
+    BicubicInterpolation(DataPoint[][] points){
+        BicubicInterpolator bicubic = new BicubicInterpolator();
 
         //extracting the information from the fake data points
-        double[] lats = new double[4];
-        double[] longs = new double[4];
-        double[][] temps = new double[4][4];
-
-        for(int i = 0; i < 4; i++){
+        int rowlen = points[0].length;
+        double[] lats = new double[rowlen];
+        double[] longs = new double[rowlen];
+        double[][] temps = new double[rowlen][rowlen];
+        
+        for(int i = 0; i < rowlen; i++){
             lats[i] = points[i][0].getLatitude();
         }
 
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < rowlen; i++){
             longs[i] = points[0][i].getLongitude();
         }
-
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
+        
+        for(int i = 0; i < rowlen; i++){
+            for(int j = 0; j < rowlen; j++){
                 temps[i][j] = points[i][j].getTemperatureK();
             }
         }
 
-        BicubicInterpolatingFunction bicubicfunc = bicubic.interpolate(lats, longs, temps);
+        bicubicfunc = bicubic.interpolate(lats, longs, temps);
 
-        //value at this point should be about 327.5
-        double interpolated = bicubicfunc.value(60.55, 60.55);
-        System.out.println(interpolated);
+    }
+
+    public double interpolate(double x, double y){
+        return bicubicfunc.value(x,y);
     }
 
     public static void main(String args[]){
-        BicubicInterpolation interpolation = new BicubicInterpolation();
+        //creating fake data points
+        DataPoint[][] points = new DataPoint[4][4];
+        float latitude = 60;
+        float longitude = 60;
+        float tempk = 300;
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                points[i][j] = new DataPoint(latitude, longitude, tempk);
+                latitude += 0.5;
+                tempk += 5;
+            }
+            longitude += 0.5;
+        }
+
+        BicubicInterpolation interpolator = new BicubicInterpolation(points);
+        //should be about 327.5
+        System.out.println(interpolator.interpolate(60.55, 60.55));
     }
 }
