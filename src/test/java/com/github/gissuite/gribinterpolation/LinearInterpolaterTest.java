8e7ec3a78cc38d447b628fb99bb6ae2022b29b78
Anna <anna.valdez@selu.edu>
2024-03-23 20:26:52 -0500

    Naming
    naming changes

--- src/test/java/com/github/gissuite/gribinterpolation/LinearInterpolaterTest.java
+++ src/test/java/com/github/gissuite/gribinterpolation/LinearInterpolaterTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LinearInterpolaterTest {
+public class LinearInterpolaterTests {
     @Test
-    public void exampleTest() {
+    public void linearInterpolation_Should_Return_DataPoint() {
         DataPoint first = new DataPoint(-62, 38.8f, 271.4f, 300);
         DataPoint second = new DataPoint(-62, 38.8f, 273.3f, 400);
         DataPoint expectedResult = new DataPoint(-62, 38.8f, 272.44498f, 355);
