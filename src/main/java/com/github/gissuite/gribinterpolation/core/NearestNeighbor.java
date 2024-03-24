3352d31110bc297b49b76f176acbc47ab7e56eae
Alyssa Major <alyssamajor2020@gmail.com>
2024-03-23 23:00:57 -0500

    we close to returning a datapoint

--- src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
@@ -4,9 +4,8 @@ import com.github.gissuite.gribinterpolation.data.DataPoint;
 
 import java.util.*;
 
-
 public class NearestNeighbor {
-    public static HashMap<DataPoint,Double>sortByValue(HashMap<DataPoint,Double>map) {
+    public static HashMap<DataPoint, Double> sortByValue(HashMap<DataPoint, Double> map) {
         List<Map.Entry<DataPoint, Double>> list = new LinkedList<>(map.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<DataPoint, Double>>() {
             @Override
@@ -15,79 +14,50 @@ public class NearestNeighbor {
             }
         });
         HashMap<DataPoint, Double> sortedMap = new LinkedHashMap<>();
-        for(Map.Entry<DataPoint,Double> entry : list){
+        for (Map.Entry<DataPoint, Double> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
 
         }
         return sortedMap;
     }
-    public static void main(String[] args) {
+//    public static DataPoint[] dataPointAtHashMapIndex(HashMap<DataPoint, Double> map){
+//
+//            System.out.println(map.keySet());
+////            nearestNeighbors[j] =sortedHashMap.keySet();
+//
+//
+//    }
 
-//        datapoints[0] = {lat,long,temp,depth}
-//        DataPoint a = new DataPoint(1,1,5,1);
+    public static void main(String[] args) {
         DataPoint first = new DataPoint(-66, 38.8f, 271.4f, 300);
         DataPoint second = new DataPoint(-62, 38.8f, 273.3f, 400);
         DataPoint expectedResult = new DataPoint(-62, 38.8f, 272.44498f, 355);
-        DataPoint[] arrayOfDataPoints = {first,second};
-//        DataPoint dataPointToInterpolate = {-62, 38.8f, 355 };
-        getNearestNeighbor(arrayOfDataPoints, -62,38.8f, 2,2);
-    }//end main
-    public static void getNearestNeighbor(DataPoint[] dataPoint, float longitudeToInterpolate, float latitudeToInterpolate, int k, int amountOfDataPoints) {
+        DataPoint[] arrayOfDataPoints = {first, second};
+        getNearestNeighbor(arrayOfDataPoints, -62, 38.8f, 2, 2);
+    }
+
+    public static DataPoint[] getNearestNeighbor(DataPoint[] dataPoint, float longitudeToInterpolate, float latitudeToInterpolate, int k, int amountOfDataPoints) {
         DataPoint[] nearestNeighbors = new DataPoint[k];
         double[] distancesFromPointInterpolating = new double[k];
         double maxValue = Double.MAX_VALUE;
         HashMap<DataPoint, Double> hashMap = new HashMap<>();
-
         for (int i = 0; i < amountOfDataPoints; i++) {
-            double distance = DistanceFinder.haverSine(dataPoint[i].getLatitude(), dataPoint[i].getLongitude(), latitudeToInterpolate,longitudeToInterpolate);
+            double distance = DistanceFinder.haverSine(dataPoint[i].getLatitude(), dataPoint[i].getLongitude(), latitudeToInterpolate, longitudeToInterpolate);
             hashMap.put(dataPoint[i], distance);
 //            System.out.println(hashMap.get(dataPoint[i]));
-            HashMap sortedHashMap = sortByValue(hashMap);
-            System.out.println(sortedHashMap.values());
+        }
+        System.out.println(hashMap.values());
+        HashMap sortedHashMap = sortByValue(hashMap);
+        System.out.println(sortedHashMap.values());
+        for(int j = 0; j < k; j++){
+//            System.out.println(sortedHashMap.keySet());
+            Set<DataPoint> keySet = hashMap.keySet();
+            for (DataPoint key : keySet) {
+                System.out.println(key);
+            }
+//            nearestNeighbors[j] =sortedHashMap.keySet();
+        }
+        return nearestNeighbors;
+    }
+}
 
-            //just need to find a way to order hash map by distance.
-//            if (distance < maxValue){
-//                maxValue = distance;
-
-
-                //create a map from datapoint to double(of distances), sort by map[pointc] - map[pointb].. then take first k elements
-//
-//
-//                nearestNeighbors[i][] = closetPoint; //function to get lat/long/depth/temp and put in array of nearestNeighbors
-
-//                distancesFromPointInterpolating[i] = distance;
-//                if (distance < distancesFromPointInterpolating[i]){
-//                    distancesFromPointInterpolating[i] = distance;
-//                            //maybe a sort to sort all the points in acending order based on distances????
-                }
-
-//            }
-//            else if (distance > maxValue) {
-//                //do nothing
-//            }
-            }//end nearest neighbor
-
-//        return nearestNeighbors;
-    }//end class
-
-
-//       for (int i = 0; i < points.length; i++) {
-//double distance = DistanceFinder.haverSine(points[i][0], points[0][i], Pointinterpolating[0],Pointinterpolating[1]);
-////            dist1 = getDistanceXYZ(points[i], mypoint);
-//            if (dist1 == dist2) {
-//closestPoints[i] = dist1;
-//closestPoints[i+1] = dist2;
-//counter++; //need to find a way to tell if a point in array is null so that closestPoints[i]
-//        //doesn't get overwritten
-//        } else if (dist1 < dist2) {
-//dist1 = dist1;
-//closestPoints[i] = dist1;
-//
-//            } else if (dist1 > dist2) {
-//
-//closestPoints[i] = dist1;
-//            }
-//                    }
-//                    return closestPoints;
-//    }
-//            }
