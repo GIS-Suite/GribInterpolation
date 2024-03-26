4a44701368542ddc59f1749ddbe75f768571cbae
Samuel Echols <samuel.echols@selu.edu>
2024-03-26 11:13:35 -0500

    feat: added documentation

--- src/main/java/com/github/gissuite/gribinterpolation/core/interpolation/LinearInterpolator.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/interpolation/LinearInterpolator.java
@@ -3,10 +3,19 @@ package com.github.gissuite.gribinterpolation.core.interpolation;
 import com.github.gissuite.gribinterpolation.data.DataPoint;
 
 public class LinearInterpolator {
+
+    /**
+     * Returns a new {@link DataPoint} that is the result of linearly interpolating between the two given data points.
+     *
+     * @param datapoint1 the first data point
+     * @param datapoint2 the second data point
+     * @param wanted the depth value at which to interpolate
+     * @return a new data point that is the result of linearly interpolating between the two given data points
+     */
     public static DataPoint interpolate(DataPoint datapoint1, DataPoint datapoint2, float wanted){
-        org.apache.commons.math3.analysis.interpolation.LinearInterpolator equation = new org.apache.commons.math3.analysis.interpolation.LinearInterpolator();
-        double[] depth = {datapoint1.getDepth(), datapoint2.getDepth()};
-        double[] temp = {datapoint1.getTemperatureK(), datapoint2.getTemperatureK()};
-        return new DataPoint(datapoint1.getLongitude(), datapoint1.getLatitude(), (float)equation.interpolate(depth, temp).value(wanted), wanted);
+        org.apache.commons.math3.analysis.interpolation.LinearInterpolator interpolator = new org.apache.commons.math3.analysis.interpolation.LinearInterpolator();
+        double[] depths = {datapoint1.getDepth(), datapoint2.getDepth()};
+        double[] temps = {datapoint1.getTemperatureK(), datapoint2.getTemperatureK()};
+        return new DataPoint(datapoint1.getLongitude(), datapoint1.getLatitude(), (float)interpolator.interpolate(depths, temps).value(wanted), wanted);
     }
 }
