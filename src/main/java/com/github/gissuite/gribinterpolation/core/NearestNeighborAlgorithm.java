0eead539c698c648bc82612e5a635fec696e5ef8
Aubrey M <you@example.com>
2024-04-02 17:44:44 -0500

    looking at some imports from tribuo

--- src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighborAlgorithm.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighborAlgorithm.java
@@ -4,9 +4,13 @@ import com.github.gissuite.gribinterpolation.data.DataPoint;
 
 import java.util.Arrays;
 
+import com.oracle.labs.mlrg.olcut.provenance.ProvenanceUtil;
+import org.tribuo.MutableDataset;
 import org.tribuo.common.nearest.KNNModel;
 import org.tribuo.common.nearest.KNNTrainer;
+import org.tribuo.evaluation.TrainTestSplitter;
 import org.tribuo.regression.Regressor;
+import org.tribuo.regression.evaluation.RegressionEvaluator;
 
 import static com.github.gissuite.gribinterpolation.core.NearestNeighbor.getNearestNeighbor;
 import static ucar.nc2.util.rc.RC.log;
@@ -17,8 +21,7 @@ public class NearestNeighborAlgorithm {
 
         nearestNeighbor.testSet();
         // nearestNeighbor.createDatasets();
-        // nearestNeighbor.trainAndEvaluate();
-        // nearestNeighbor.saveModel();
+        nearestNeighbor.trainAndEvaluate();
     } // end main
 
     public static float nearestNeighborAlgorithm ( int k, DataPoint[] nearestNeighbors){
@@ -30,7 +33,11 @@ public class NearestNeighborAlgorithm {
 
 
     } // end nearestNeighborAlgorithm
+    protected void createTrainer() {
+        log.info("Creating trainer....");
 
+
+    }
 
     protected void testSet(){
         log.info("Creating a test set....");
@@ -38,17 +45,18 @@ public class NearestNeighborAlgorithm {
         DataPoint first = new DataPoint(-66, 38.8f, 271.4f, 300);
         DataPoint second = new DataPoint(-62, 38.8f, 273.3f, 400);
         DataPoint third = new DataPoint(-68, 38.8f, 273.3f, 400);
-        /// DataPoint expectedResult = new DataPoint(-62, 38.8f, 272.44498f, 355);
+        // DataPoint expectedResult = new DataPoint(-62, 38.8f, 272.44498f, 355);
 
         // putting test data into an array
         DataPoint[] arrayOfDataPoints = {first, second, third};
-
         DataPoint[] nearestNeighbors = getNearestNeighbor(arrayOfDataPoints, -62, 38.8f, 2, 3);
         System.out.println(Arrays.toString(nearestNeighbors)); //prints out the location of the DataPoints
 
         //can get temperature this way:
         DataPoint a = nearestNeighbors[0];
         System.out.println(a.getTemperatureK());
+
+
     }
 
     // getting our test set in a usable state for tribuo
@@ -57,5 +65,19 @@ public class NearestNeighborAlgorithm {
         
     }
 
+    private void evaluate(){
+        log.info("Evaluating datasets....");
+        RegressionEvaluator evaluator = new RegressionEvaluator();
+    }
 
+    private void trainingAndEvaluate() {
+        log.info("Training model...");
+
+        evaluate(model, "testSet", testSet);
+
+        log.info("Dataset Provenance: --------------------");
+        log.info(ProvenanceUtil.formattedProvenanceString(model.getProvenance().getDatasetProvenance()));
+        log.info("Trainer Provenance: --------------------");
+        log.info(ProvenanceUtil.formattedProvenanceString(model.getProvenance().getTrainerProvenance()));
+    }
 } // end NearestNeighborAlgorithm
