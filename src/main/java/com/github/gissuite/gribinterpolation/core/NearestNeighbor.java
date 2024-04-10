d145d4eecb6f0a750384d9bf4406a671c9baabc5
Alyssa Major <alyssamajor2020@gmail.com>
2024-04-09 23:04:43 -0500

    added documentation and did the changes

--- src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
@@ -4,31 +4,42 @@ import com.github.gissuite.gribinterpolation.data.DataPoint;
 import java.util.*;
 import java.util.stream.Collectors;
 
+//gets k nearest neighbors' memory locations stored in a DataPoint array
+//amountOfDataPoints is how many points to find the distance for; so should be how many DataPoints in total we have.
+//k is how many neighbors to look for
+
+//can get temperature this way:
+//ArrayList<DataPoint> nearestNeighbors = (getNearestNeighbor(arrayOfDataPoints, longitudeToInterpolate, latitudeToInterpolate, k, amountOfDataPoints));
+//DataPoint a = (DataPoint) nearestNeighbors.get(0);
+//System.out.println(a.getTemperatureK());
 public class NearestNeighbor {
+
+        public static ArrayList<DataPoint> getNearestNeighbor(ArrayList<DataPoint> dataPoints, float longitudeToInterpolate, float latitudeToInterpolate, int k, int amountOfDataPoints) {
+            ArrayList<DataPoint> nearestNeighbors = new ArrayList<>();
+            HashMap<DataPoint, Double> hashMap = new HashMap<>();
+            for (int i = 0; i < amountOfDataPoints; i++) {
+                double distance = DistanceFinder.haverSine(dataPoints.get(i).getLatitude(), dataPoints.get(i).getLongitude(), latitudeToInterpolate, longitudeToInterpolate);
+                hashMap.put(dataPoints.get(i), distance);
+            }
+            Map<DataPoint, Double> sortedHashMap = sortByValue(hashMap);
+            Set<DataPoint> keySet = sortedHashMap.keySet();
+            Iterator<DataPoint> iterator = keySet.iterator();
+            int count = 0;
+            while (iterator.hasNext() && count < k) {
+                DataPoint key = iterator.next();
+                nearestNeighbors.add(key);
+                count++;
+            }
+        return nearestNeighbors;
+        }
+
 private static Map<DataPoint, Double> sortByValue(HashMap<DataPoint, Double> map) {
     return map.entrySet()
             .stream()
             .sorted(Map.Entry.comparingByValue())
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (firstValue, streamValue) -> firstValue, LinkedHashMap::new));
 }
-    public static DataPoint[] getNearestNeighbor(DataPoint[] dataPoint, float longitudeToInterpolate, float latitudeToInterpolate, int k, int amountOfDataPoints) {
-        DataPoint[] nearestNeighbors = new DataPoint[k];
-        HashMap<DataPoint, Double> hashMap = new HashMap<>();
-        for (int i = 0; i < amountOfDataPoints; i++) {
-            double distance = DistanceFinder.haverSine(dataPoint[i].getLatitude(), dataPoint[i].getLongitude(), latitudeToInterpolate, longitudeToInterpolate);
-            hashMap.put(dataPoint[i], distance);
-        }
-        Map<DataPoint, Double> sortedHashMap = sortByValue(hashMap);
-        Set<DataPoint> keySet = sortedHashMap.keySet();
-        Iterator<DataPoint> iterator = keySet.iterator();
-        int count = 0;
-        while(iterator.hasNext() && count < k){
-            DataPoint key = iterator.next();
-            nearestNeighbors[count] = key;
-            count++;
-        }
-        return nearestNeighbors;
+
     }
 
-}
 
