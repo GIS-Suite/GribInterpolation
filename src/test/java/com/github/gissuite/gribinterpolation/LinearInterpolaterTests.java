62dc58635c4f59ee33bda5bfc857da9f9bdc9fd2
Derrick <zhinkee@gmail.com>
2024-04-20 13:48:42 -0500

    LinearInt updates:
    -This update corresponds to the latest program structure discussed-
    1. LinearInt takes in 3 data points: the upper depth data point, lower depth data point, and the target data point(missing temperature value). the interpolation method interpolates the temperature for the target data point and returns the target data point.
    2. Variable naming

--- src/test/java/com/github/gissuite/gribinterpolation/LinearInterpolaterTests.java
+++ src/test/java/com/github/gissuite/gribinterpolation/LinearInterpolaterTests.java
@@ -4,16 +4,18 @@ import com.github.gissuite.gribinterpolation.core.LinearInt;
 import com.github.gissuite.gribinterpolation.data.DataPoint;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Float.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LinearInterpolaterTests {
     @Test
     public void linearInterpolation_Should_Return_DataPoint() {
-        DataPoint first = new DataPoint(-62, 38.8f, 271.4f, 300);
-        DataPoint second = new DataPoint(-62, 38.8f, 273.3f, 400);
+        DataPoint upperDepthDataPoint = new DataPoint(-62, 38.8f, 271.4f, 300);
+        DataPoint lowerDepthDataPoint = new DataPoint(-62, 38.8f, 273.3f, 400);
+        DataPoint targetDataPoint = new DataPoint(-62, 38.8f, NaN, 355);
         DataPoint expectedResult = new DataPoint(-62, 38.8f, 272.44498f, 355);
 
-        DataPoint result = LinearInt.interpolate(first, second, 355);
+        DataPoint result = LinearInt.interpolate(upperDepthDataPoint, lowerDepthDataPoint, targetDataPoint);
         System.out.print(result.getLongitude() + " " + result.getLatitude() + " " + result.getTemperatureK() + " " + result.getDepth());
         assertEquals(expectedResult.getTemperatureK(), result.getTemperatureK());
     }
