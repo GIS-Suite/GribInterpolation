6ef9d2bdea4aa593f7a281b3568cb45fe02f6916
Derrick Poon <zhinkee@gmail.com>
2024-04-22 18:14:11 -0500

    DatasetLinearInterpolation:
    1. Group all data points by latitude and latitude.
    2. filter out group of data points with all NaN temperatures.

--- src/main/java/com/github/gissuite/gribinterpolation/core/DatasetLinearInterpolation.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/DatasetLinearInterpolation.java
@@ -1,11 +1,14 @@
 package com.github.gissuite.gribinterpolation.core;
 
 import com.github.gissuite.gribinterpolation.data.DataPoint;
+import org.apache.commons.math3.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.*;
+
+import static java.lang.Float.NaN;
 
 public class DatasetLinearInterpolation {
     /**
@@ -13,15 +16,24 @@ public class DatasetLinearInterpolation {
      * @return The ArrayList of all the data points with NaN-temperature replaced with linear interpolated temperature
      */
     public static ArrayList<DataPoint> dataPointsLinearInterpolation(ArrayList<DataPoint> dataPointArrayList){
-        //group all data points by latitude
-        Map<Float, List<DataPoint>> dataPointByLatitude = dataPointArrayList
+        //group all data points by latitude and longitude
+        Map<Pair<Float, Float>, List<DataPoint>> allDataPointsByLatLon = dataPointArrayList
                 .stream()
                 .collect(
-                        Collectors.groupingBy(DataPoint::getLatitude)
+                        Collectors.groupingBy(
+                                dp -> new Pair<>(dp.getLatitude(),dp.getLongitude())
+                        )
                 );
 
-        //filter out group of data points with all NaN temperatures
+        //Test print (DELETE LATER)
+        for (Map.Entry<?, ?> entry : allDataPointsByLatLon.entrySet()) {
+            System.out.printf("%-15s : %s%n", entry.getKey(), entry.getValue());
+        }
 
+        //filter group of data points with non NaN temperatures
+//        Map<Pair<Float, Float>, List<DataPoint>> dataPointsByLatLonWithTemp = allDataPointsByLatLon.entrySet()
+//                .stream()
+//                .filter( ->  )
 
         return dataPointArrayList;
     }
--- src/test/java/com/github/gissuite/gribinterpolation/DatasetLinearInterpolationTests.java
+++ src/test/java/com/github/gissuite/gribinterpolation/DatasetLinearInterpolationTests.java
@@ -6,31 +6,32 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static com.github.gissuite.gribinterpolation.core.DatasetLinearInterpolation.dataPointsLinearInterpolation;
+import static java.lang.Float.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DatasetLinearInterpolationTests {
     @Test
     public void datasetLinearInterpolation_Should_Return_DataPointArrayList(){
         //Data points that represent the dataset
-        DataPoint pointA0 = new DataPoint(100, 10, 51, 0);
-        DataPoint pointB0 = new DataPoint(100, 11, 50, 0);
-        DataPoint pointC0 = new DataPoint(100, 12, 49, 0);
-        DataPoint pointD0 = new DataPoint(100, 13, 48, 0);
+        DataPoint pointA0 = new DataPoint(100, -90, 51, 0);
+        DataPoint pointB0 = new DataPoint(101, -90, 49, 0);
+        DataPoint pointC0 = new DataPoint(102, -90, 50, 0);
+        DataPoint pointD0 = new DataPoint(103, -90, 48, 0);
 
-        DataPoint pointA3 = new DataPoint(100, 10, 48, 3);
-        DataPoint pointB3 = new DataPoint(100, 11, 47, 3);
-        DataPoint pointC3 = new DataPoint(100, 12, 46, 3);
-        DataPoint pointD3 = new DataPoint(100, 13, 45, 3);
+        DataPoint pointA3 = new DataPoint(100, -90, 48, 3);
+        DataPoint pointB3 = new DataPoint(101, -90, 46, 3);
+        DataPoint pointC3 = new DataPoint(102, -90, 47, 3);
+        DataPoint pointD3 = new DataPoint(103, -90, 45, 3);
 
-        DataPoint pointE0 = new DataPoint(101, 10, 49.9f, 0);
-        DataPoint pointF0 = new DataPoint(101, 11, 48.9f, 0);
-        DataPoint pointG0 = new DataPoint(101, 12, 47.9f, 0);
-        DataPoint pointH0 = new DataPoint(101, 13, 46.9f, 0);
+        DataPoint pointE0 = new DataPoint(100, -89, NaN, 0);
+        DataPoint pointF0 = new DataPoint(101, -89, NaN, 0);
+        DataPoint pointG0 = new DataPoint(102, -89, NaN, 0);
+        DataPoint pointH0 = new DataPoint(103, -89, NaN, 0);
 
-        DataPoint pointE2 = new DataPoint(101, 10, 48.9f, 2);
-        DataPoint pointF2 = new DataPoint(101, 11, 47.9f, 2);
-        DataPoint pointG2 = new DataPoint(101, 12, 46.9f, 2);
-        DataPoint pointH2 = new DataPoint(101, 13, 45.9f, 2);
+        DataPoint pointE2 = new DataPoint(100, -89, 48.9f, 2);
+        DataPoint pointF2 = new DataPoint(101, -89, 47.9f, 2);
+        DataPoint pointG2 = new DataPoint(102, -89, 46.9f, 2);
+        DataPoint pointH2 = new DataPoint(103, -89, 45.9f, 2);
 
         //add all data points to an ArrayList
         ArrayList<DataPoint> dataPointArrayList = new ArrayList<>();
