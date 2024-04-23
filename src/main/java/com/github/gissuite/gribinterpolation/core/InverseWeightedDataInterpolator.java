dbbadd4f2a01710aaac1f73da4d18fdbc7d7ff29
allielemmond <allie.lemmond@gmail.com>
2024-04-23 17:52:22 -0500

    start datainterpolator

---
+++ src/main/java/com/github/gissuite/gribinterpolation/core/InverseWeightedDataInterpolator.java
@@ -0,0 +1,40 @@
+package com.github.gissuite.gribinterpolation.core;
+
+import com.github.gissuite.gribinterpolation.data.DataPoint;
+
+import java.util.ArrayList;
+
+import static java.lang.Float.NaN;
+
+public class InverseWeightedDataInterpolator {
+//        /**
+//     * @param dataPoints An array list of all the data points with known and unknown temperatures
+//     *
+//     * @return the dataPoints array list with all the unknown temperature values replaced with interpolated values
+//     */
+    public static ArrayList<DataPoint> DataInterpolator(ArrayList<DataPoint> dataPoints){
+
+        //create 2 arraylists of points with known and unknown temperatures
+        ArrayList<DataPoint> knownDataPoints = new ArrayList<>();
+        ArrayList<DataPoint> pointsToInterpolate = new ArrayList<>();
+        for (DataPoint dataPoint : dataPoints){
+//            if (Float.isNaN(dataPoint.getTemperatureK())) {
+//                pointsToInterpolate.add(dataPoint);
+//                System.out.println("Point to interpolate " + dataPoint.getLongitude());
+//            }
+//            else {
+//                knownDataPoints.add(dataPoint);
+//                System.out.println("Point with known temperature " + dataPoint.getLongitude());
+//            }
+        }
+
+//        //call the inverseWeighted interpolation method finding the
+//        //temperatures of pointsToInterpolate using knownDataPoints
+//        for (DataPoint interpolatePoint : pointsToInterpolate) {
+//            InverseWeighted.inverseWeighted(knownDataPoints, interpolatePoint);
+//        }
+//
+//        //return updated arraylist
+//        return dataPoints;
+//    }
+}
