86b29407afcdce8ceddfd6739322cf9ddae1cb53
Aubrey M <you@example.com>
2024-04-24 09:27:35 -0500

    Fixed test
    I was overcomplicating things

--- src/test/java/com/github/gissuite/gribinterpolation/NearestNeighborTest.java
+++ src/test/java/com/github/gissuite/gribinterpolation/NearestNeighborTest.java
@@ -1,21 +1,19 @@
 package com.github.gissuite.gribinterpolation;
-import com.github.gissuite.gribinterpolation.core.BilinearInterpolator;
+
 import com.github.gissuite.gribinterpolation.core.NearestNeighbor;
 import com.github.gissuite.gribinterpolation.data.DataPoint;
-import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
 
-import static com.github.gissuite.gribinterpolation.core.NearestNeighbor.getNearestNeighbor;
-import static com.github.gissuite.gribinterpolation.core.NearestNeighbor.knnInterpolation;
+import java.util.ArrayList;
 import static java.lang.Float.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.logging.Logger;
 
 public class NearestNeighborTest {
+    Logger logger = Logger.getLogger(NearestNeighborTest.class.getName());
+    public static void main(String[] args) {}
     @Test
     public void knn_Should_Return_DataPoint(){
-
         // testing data
         ArrayList<DataPoint> neighbors = new ArrayList<>();
         neighbors.add(new DataPoint(3, 3, 37, -4));
@@ -24,48 +22,17 @@ public class NearestNeighborTest {
         neighbors.add(new DataPoint(2, 3, 90, -3));
 
         // Creating point that we are interpolating
-        DataPoint interpolatedPoint = new DataPoint(2, 3, NaN, -4);
+        DataPoint interpolatingPoint = new DataPoint(2, 3, NaN, -4);
+        // expected result to compare
+        DataPoint expectedResultDataPoint = new DataPoint(2, 3, 56, -4);
 
         try{
-            // testing only ArrayList<DataPoint> getNearestNeighbor
-            try{
-                // Interpolation point coordinates
-                float latitudeToInterpolate = 2;
-                float longitudeToInterpolate = 3;
-                // Number of nearest neighbors to find
-                int k = 3;
-                // Call the method to get nearest neighbors
-                ArrayList<DataPoint> nearestNeighbors = NearestNeighbor.getNearestNeighbor(neighbors, longitudeToInterpolate, latitudeToInterpolate, k, neighbors.size());
-                // Assert the result
-                System.out.println("Nearest Neighbors:");
-                for (DataPoint neighbor : nearestNeighbors) {
-                    System.out.println("Longitude: " + neighbor.getLongitude() + ", Latitude: " + neighbor.getLatitude() + ", Temperature: " + neighbor.getTemperatureK() + ", Depth: " + neighbor.getDepth());
-                }
-            }
-            catch(Exception e){
-                System.out.println("Problem with getNearestNeighbor:" + e);
-            }
-            // testing knnInterpolation
-            try{
-                // Find k nearest neighbor
-                int k = 3;
-                ArrayList<DataPoint> nearestNeighbors = getNearestNeighbor(neighbors, interpolatedPoint.getLongitude(), interpolatedPoint.getLatitude(),  k, neighbors.size());
-
-                // KNN interpolation for temperature
-                double interpolatedTemp = knnInterpolation(nearestNeighbors, interpolatedPoint);
-
-                // Setting the temperature
-                interpolatedPoint.setTemperatureK((float) interpolatedTemp);
-
-                // Showing the new temperature
-                System.out.println("Interpolated Temp: " + interpolatedPoint.getTemperatureK());
-            }
-            catch (Exception e) {
-                System.out.println("Problem with knnInterpolation:" + e);
-            }
+            int k = 3;
+            double resultTemp = NearestNeighbor.knnInterpolation(neighbors, interpolatingPoint, k);
+            interpolatingPoint.setTemperatureK((float)resultTemp);
+            assertEquals(expectedResultDataPoint.getTemperatureK(), interpolatingPoint.getTemperatureK());
         }
-        catch(Exception e){
-            System.out.println("Error with the overall :( : " + e);
-        }
+        catch (Exception e){ logger.warning("Error with interpolation" + e); }
+
     }
 }
