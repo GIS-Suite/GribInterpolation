ca02659815e04547f27ee965acbd12844ce8db45
allielemmond <allie.lemmond@gmail.com>
2024-04-24 21:02:02 -0500

    a lot of changing

--- src/main/java/com/github/gissuite/gribinterpolation/core/InverseWeightedDataInterpolator.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/InverseWeightedDataInterpolator.java
@@ -3,6 +3,9 @@ package com.github.gissuite.gribinterpolation.core;
 import com.github.gissuite.gribinterpolation.data.DataPoint;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static java.lang.Float.NaN;
 
 public class InverseWeightedDataInterpolator {
         /**
@@ -13,22 +16,43 @@ public class InverseWeightedDataInterpolator {
     public static ArrayList<ArrayList<ArrayList<DataPoint>>> DataInterpolator(ArrayList<ArrayList<ArrayList<DataPoint>>> dataPoints){
 
         //create 2 arraylists of points with known and unknown temperatures
-        ArrayList<DataPoint> knownDataPoints = new ArrayList<>();
-        ArrayList<DataPoint> pointsToInterpolate = new ArrayList<>();
+//        ArrayList<DataPoint> knownDataPoints = new ArrayList<>();
+//        ArrayList<DataPoint> pointsToInterpolate = new ArrayList<>();
 
         //extrapolate data points and allocate to the correct list
-        for (ArrayList<ArrayList<DataPoint>> dataPoints1 : dataPoints) {
-            for(ArrayList<DataPoint> points : dataPoints1){
-                for (DataPoint dataPoint : points){
-                    if (Float.isNaN(dataPoint.getTemperatureK())) {
-                        pointsToInterpolate.add(dataPoint);
-                    }
-                    else {
-                        knownDataPoints.add(dataPoint);
-                    }
-                }
-            }
-        }
+
+//        List<Integer> filteredList = array3D.stream()
+//                .flatMap(List::stream) // Flatten the outermost dimension
+//                .flatMap(List::stream) // Flatten the second dimension
+//                .filter(value -> value > threshold)
+//                .collect(Collectors.toList());
+
+        ArrayList<DataPoint> pointsToInterpolate = (ArrayList<DataPoint>) dataPoints.stream()
+                .flatMap(ArrayList::stream)
+                .flatMap(ArrayList::stream)
+                .filter(dataPoint -> Float.isNaN(dataPoint.getTemperatureK()))
+                .collect(Collectors.toList());
+
+        ArrayList<DataPoint> knownDataPoints = (ArrayList<DataPoint>) dataPoints.stream()
+                .flatMap(ArrayList::stream)
+                .flatMap(ArrayList::stream)
+                .filter(dataPoint -> !Float.isNaN(dataPoint.getTemperatureK()))
+                .collect(Collectors.toList());
+
+
+//
+//        for (ArrayList<ArrayList<DataPoint>> dataPoints1 : dataPoints) {
+//            for(ArrayList<DataPoint> points : dataPoints1){
+//                for (DataPoint dataPoint : points){
+//                    if (Float.isNaN(dataPoint.getTemperatureK())) {
+////                        pointsToInterpolate.add(dataPoint);
+//                    }
+//                    else {
+//                        knownDataPoints.add(dataPoint);
+//                    }
+//                }
+//            }
+//        }
 
 
         //call the inverseWeighted interpolation method finding the
