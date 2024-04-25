8b50004afd6fae7583e874ac92d846bcbff39a32
allielemmond <allie.lemmond@gmail.com>
2024-04-24 21:02:54 -0500

    reducing

--- src/main/java/com/github/gissuite/gribinterpolation/core/InverseWeightedDataInterpolator.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/InverseWeightedDataInterpolator.java
@@ -16,17 +16,7 @@ public class InverseWeightedDataInterpolator {
     public static ArrayList<ArrayList<ArrayList<DataPoint>>> DataInterpolator(ArrayList<ArrayList<ArrayList<DataPoint>>> dataPoints){
 
         //create 2 arraylists of points with known and unknown temperatures
-//        ArrayList<DataPoint> knownDataPoints = new ArrayList<>();
-//        ArrayList<DataPoint> pointsToInterpolate = new ArrayList<>();
-
         //extrapolate data points and allocate to the correct list
-
-//        List<Integer> filteredList = array3D.stream()
-//                .flatMap(List::stream) // Flatten the outermost dimension
-//                .flatMap(List::stream) // Flatten the second dimension
-//                .filter(value -> value > threshold)
-//                .collect(Collectors.toList());
-
         ArrayList<DataPoint> pointsToInterpolate = (ArrayList<DataPoint>) dataPoints.stream()
                 .flatMap(ArrayList::stream)
                 .flatMap(ArrayList::stream)
@@ -38,21 +28,6 @@ public class InverseWeightedDataInterpolator {
                 .flatMap(ArrayList::stream)
                 .filter(dataPoint -> !Float.isNaN(dataPoint.getTemperatureK()))
                 .collect(Collectors.toList());
-
-
-//
-//        for (ArrayList<ArrayList<DataPoint>> dataPoints1 : dataPoints) {
-//            for(ArrayList<DataPoint> points : dataPoints1){
-//                for (DataPoint dataPoint : points){
-//                    if (Float.isNaN(dataPoint.getTemperatureK())) {
-////                        pointsToInterpolate.add(dataPoint);
-//                    }
-//                    else {
-//                        knownDataPoints.add(dataPoint);
-//                    }
-//                }
-//            }
-//        }
 
 
         //call the inverseWeighted interpolation method finding the
