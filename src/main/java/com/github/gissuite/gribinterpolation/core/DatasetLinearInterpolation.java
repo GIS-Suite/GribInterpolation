567c3f4b0a4503aebbf5c10be3f6da46f93025b5
Derrick <zhinkee@gmail.com>
2024-04-27 20:21:29 -0500

    DatasetLinearInterpolation Complete:
    1. The Dataset gets grouped by latitude and longitude, filtered of all-NaN temperature groups, and sorted by depth in each group.
    
    2. The dataPointsLinearInterpolation method traverses through each latitude and longitude group and finds interpolated temperature at all the missing depths.
    
    EdgeCases: NaN temperatures at the start of the list, middle of the list, and end of the list are taken care of.

--- src/main/java/com/github/gissuite/gribinterpolation/core/DatasetLinearInterpolation.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/DatasetLinearInterpolation.java
@@ -17,6 +17,8 @@ public class DatasetLinearInterpolation {
      * @return The ArrayList of all the data points with NaN-temperature replaced with linear interpolated temperature
      */
     public static ArrayList<DataPoint> dataPointsLinearInterpolation(ArrayList<DataPoint> dataPointArrayList){
+        ArrayList<DataPoint> interpolatedDataPointArrayList = new ArrayList<>();
+
         // shape the dataset
         Map<Pair<Float, Float>, List<DataPoint>> dataPointsMap = groupByLatLonWithDepthSort(dataPointArrayList);
 
@@ -29,21 +31,20 @@ public class DatasetLinearInterpolation {
             List<DataPoint> groupedDataPointsList = latLonKey.getValue();
 
             int listIndex = 0;
-            int nextListIndex;
             int upperBoundIndex = 0;
-            int lowerBoundIndex = 0;
+            int lowerBoundIndex;
             DataPoint upperBoundDataPoint = null;
-            DataPoint lowerBoundDataPoint = null;
+            DataPoint lowerBoundDataPoint;
 
             while (listIndex < groupedDataPointsList.size()) {
                 DataPoint currentDataPoint = groupedDataPointsList.get(listIndex);
 
+                //edge case: Check if first Data Point temperature is NaN. If data point temperature is NaN, remove
                 if (!Float.isNaN(currentDataPoint.getTemperatureK())) {
 
                     //get upper and lower data point boundaries for linear interpolation
                     if (upperBoundDataPoint == null) {
                         upperBoundDataPoint = currentDataPoint;
-                        upperBoundIndex = listIndex;
                         listIndex++;
                     } else {
                         if((int)currentDataPoint.getDepth() == (int)upperBoundDataPoint.getDepth() + 1) {
@@ -54,7 +55,8 @@ public class DatasetLinearInterpolation {
                             lowerBoundDataPoint = currentDataPoint;
                             lowerBoundIndex = listIndex;
 
-                            nextListIndex = upperBoundIndex + 1;
+                            //
+                            int nextListIndex = upperBoundIndex + 1;
                             int numberOfMissingDepths = (int)(lowerBoundDataPoint.getDepth() - upperBoundDataPoint.getDepth()) - 1;
                             int currentTargetDepth = (int)upperBoundDataPoint.getDepth() + 1;
 
@@ -68,8 +70,7 @@ public class DatasetLinearInterpolation {
                                 //Call linear interpolation method with upper bound data point, lower bound data point, and target data point
                                 DataPoint interpolatedDataPoint = interpolate(upperBoundDataPoint, lowerBoundDataPoint, targetDataPoint);
 
-                                //if temperature of next data point in list is NaN, replace with interpolated data point
-
+                                //edge case: if temperature of next data point in list is NaN, replace with interpolated data point
                                 if (Float.isNaN(groupedDataPointsList.get(nextListIndex).getTemperatureK())){
                                     groupedDataPointsList.set(nextListIndex, interpolatedDataPoint);
                                 } else {
@@ -77,29 +78,33 @@ public class DatasetLinearInterpolation {
                                     lowerBoundIndex += 1;
                                 }
                                 nextListIndex++;
-
                                 currentTargetDepth++;
                             }
 
-                            //assign previous lowerBoundDataPoint to
-                            upperBoundDataPoint = groupedDataPointsList.get(lowerBoundIndex);
-                            lowerBoundDataPoint = null;
+                            //assign previous lowerBoundDataPoint to upperBoundDataPoint
+                            upperBoundIndex = lowerBoundIndex;
+                            upperBoundDataPoint = lowerBoundDataPoint;
                             listIndex = lowerBoundIndex + 1;
                         }
                     }
                 }
+                else {
+                    groupedDataPointsList.remove(listIndex);
+                }
             }
-
-            //sort list
 
             //update map entry with updated list
             dataPointsMap.put(latLonKey.getKey(), groupedDataPointsList);
 
+            //add updated list of data points to ArrayList
+            interpolatedDataPointArrayList.addAll(groupedDataPointsList);
         }
 
+        System.out.println("Data shape after Linear Interpolation:");
         printMap(dataPointsMap);
 
-        return dataPointArrayList;
+        //return all data points
+        return interpolatedDataPointArrayList;
     }
 
     /**
--- src/test/java/com/github/gissuite/gribinterpolation/DatasetLinearInterpolationTests.java
+++ src/test/java/com/github/gissuite/gribinterpolation/DatasetLinearInterpolationTests.java
@@ -13,20 +13,20 @@ public class DatasetLinearInterpolationTests {
     @Test
     public void datasetLinearInterpolation_Should_Return_DataPointArrayList(){
         //Data points that represent the dataset
-        DataPoint pointA0 = new DataPoint(100, -90, 51, 0);
+        DataPoint pointA0 = new DataPoint(100, -90, NaN, 0);
         DataPoint pointB2 = new DataPoint(101, -90, 49, 2);
         DataPoint pointC10 = new DataPoint(102, -90, 40, 10);
-       //DataPoint pointD11 = new DataPoint(103, -90, 39, 11);
+        DataPoint pointD11 = new DataPoint(103, -90, NaN, 11);
 
         DataPoint pointA3 = new DataPoint(100, -90, 48, 3);
         DataPoint pointB1 = new DataPoint(101, -90, 50, 1);
-        DataPoint pointC4 = new DataPoint(102, -90, 46, 4);
-        //DataPoint pointD3 = new DataPoint(103, -90, 47, 3);
+        DataPoint pointC4 = new DataPoint(102, -90, NaN, 4);
+        DataPoint pointD3 = new DataPoint(103, -90, 47, 3);
 
         DataPoint pointA7 = new DataPoint(100, -90, 44, 7);
         DataPoint pointB9 = new DataPoint(101, -90, 42, 9);
         DataPoint pointC1 = new DataPoint(102, -90, 49, 1);
-        //DataPoint pointD6 = new DataPoint(103, -90, 44, 6);
+        DataPoint pointD6 = new DataPoint(103, -90, 44, 6);
 
         DataPoint pointE0 = new DataPoint(100, -89, NaN, 0);
         DataPoint pointF0 = new DataPoint(101, -89, NaN, 22);
@@ -40,16 +40,14 @@ public class DatasetLinearInterpolationTests {
 
         //add all data points to an ArrayList unsorted
         ArrayList<DataPoint> dataPointArrayList = new ArrayList<>();
-        dataPointArrayList.add(pointA0);dataPointArrayList.add(pointB2);dataPointArrayList.add(pointC10);
-        //dataPointArrayList.add(pointD11);
-        dataPointArrayList.add(pointA3);dataPointArrayList.add(pointB1);dataPointArrayList.add(pointC4);
-        //dataPointArrayList.add(pointD3);
-        dataPointArrayList.add(pointA7);dataPointArrayList.add(pointB9);dataPointArrayList.add(pointC1);
-        //dataPointArrayList.add(pointD6);
+        dataPointArrayList.add(pointA0);dataPointArrayList.add(pointB2);dataPointArrayList.add(pointC10);dataPointArrayList.add(pointD11);
+        dataPointArrayList.add(pointA3);dataPointArrayList.add(pointB1);dataPointArrayList.add(pointC4);dataPointArrayList.add(pointD3);
+        dataPointArrayList.add(pointA7);dataPointArrayList.add(pointB9);dataPointArrayList.add(pointC1);dataPointArrayList.add(pointD6);
         dataPointArrayList.add(pointE0);dataPointArrayList.add(pointF0);dataPointArrayList.add(pointG0);dataPointArrayList.add(pointH0);
         dataPointArrayList.add(pointE2);dataPointArrayList.add(pointF2);dataPointArrayList.add(pointG2);dataPointArrayList.add(pointH2);
 
         ArrayList<DataPoint>interpolatedDataPointArrayList = dataPointsLinearInterpolation(dataPointArrayList);
+        System.out.println(interpolatedDataPointArrayList.size());
 
     }
 }
