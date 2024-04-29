c77215bb0d231e1938c6540e2536b030b015cd25
Alyssa Major <alyssamajor2020@gmail.com>
2024-04-28 20:43:09 -0500

    distance squared but prob dont need this. But i do need the abssubdept

--- src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
@@ -10,14 +10,14 @@ public class NearestNeighbor {
 
     public static void main(String[] args) {
         ArrayList<DataPoint> dataPointArrayList = new ArrayList<>(Arrays.asList(
-                new DataPoint(100, -90, Float.NaN, 0),
-                new DataPoint(101, -90, 49, 22),
-                new DataPoint(103, -90, Float.NaN, 11),
-                new DataPoint(103, -89, Float.NaN, 400)
+                new DataPoint(3, 4, Float.NaN, 4),
+                new DataPoint(3, 4, 49, 4),
+                new DataPoint(2, 3, Float.NaN, 5),
+                new DataPoint(3, 4, Float.NaN, 10)
         ));
-        DataPoint dataPointToInterpolate = new DataPoint(103, -89,40,11);
+        DataPoint dataPointToInterpolate = new DataPoint(3, 4,40,4);
         ArrayList<DataPoint> result = new ArrayList<>();
-        result = NearestNeighbor.getNearestNeighbor(dataPointArrayList,dataPointToInterpolate,2);
+        result = NearestNeighbor.getNearestNeighbor(dataPointArrayList,dataPointToInterpolate,4);
 
         ArrayList<DataPoint> expectedResult = new ArrayList<>(Arrays.asList(
                 new DataPoint(103,-90, Float.NaN, 11),
@@ -40,7 +40,7 @@ public class NearestNeighbor {
                 double distanceHaversine = DistanceFinder.haverSine(dataPoints.get(i).getLatitude(), dataPoints.get(i).getLongitude(), dataPointToInterpolate.getLatitude(), dataPointToInterpolate.getLongitude());
                 double subtractedDepth = (dataPointToInterpolate.getDepth() - dataPoints.get(i).getDepth());
                 double abssubstractedDepth = Math.abs(subtractedDepth);
-                double distanceSquared = Math.pow(distanceHaversine,2) + (Math.pow(subtractedDepth,2));
+                double distanceSquared = Math.pow(distanceHaversine,2) + (Math.pow(abssubstractedDepth,2));
                 double distance = Math.sqrt(distanceSquared);
                 hashMap.put(dataPoints.get(i), distance);
             }
