38c2b5f8afb36833927330e214a87dbd34afb61c
Alyssa Major <alyssamajor2020@gmail.com>
2024-04-28 19:48:16 -0500

    looking at depth

--- src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
+++ src/main/java/com/github/gissuite/gribinterpolation/core/NearestNeighbor.java
@@ -7,6 +7,25 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.logging.Logger;
 public class NearestNeighbor {
+
+    public static void main(String[] args) {
+        ArrayList<DataPoint> dataPointArrayList = new ArrayList<>(Arrays.asList(
+                new DataPoint(100, -90, Float.NaN, 0),
+                new DataPoint(101, -90, 49, 22),
+                new DataPoint(103, -90, Float.NaN, 11),
+                new DataPoint(103, -89, Float.NaN, 400)
+        ));
+        DataPoint dataPointToInterpolate = new DataPoint(103, -89,40,11);
+        ArrayList<DataPoint> result = new ArrayList<>();
+        result = NearestNeighbor.getNearestNeighbor(dataPointArrayList,dataPointToInterpolate,2);
+
+        ArrayList<DataPoint> expectedResult = new ArrayList<>(Arrays.asList(
+                new DataPoint(103,-90, Float.NaN, 11),
+                new DataPoint(101, -90, 49, 22)
+        ));
+//        assertEquals(expectedResult.get(0), result.get(0));
+        System.out.println(result.get(0).getDepth());
+    }
     static Logger logger = Logger.getLogger(NearestNeighbor.class.getName());
     /**
      * @param dataPoints All the DataPoints in the data set
@@ -20,8 +39,9 @@ public class NearestNeighbor {
             for (int i = 0; i < dataPoints.size(); i++) {
                 double distanceHaversine = DistanceFinder.haverSine(dataPoints.get(i).getLatitude(), dataPoints.get(i).getLongitude(), dataPointToInterpolate.getLatitude(), dataPointToInterpolate.getLongitude());
                 double subtractedDepth = (dataPointToInterpolate.getDepth() - dataPoints.get(i).getDepth());
-                double distance = Math.pow(distanceHaversine,2) + Math.pow(subtractedDepth,2);
-
+                double abssubstractedDepth = Math.abs(subtractedDepth);
+                double distanceSquared = Math.pow(distanceHaversine,2) + (Math.pow(subtractedDepth,2));
+                double distance = Math.sqrt(distanceSquared);
                 hashMap.put(dataPoints.get(i), distance);
             }
             Map<DataPoint, Double> sortedHashMap = sortByValue(hashMap);
--- src/test/java/com/github/gissuite/gribinterpolation/NearestNeighborTest.java
+++ src/test/java/com/github/gissuite/gribinterpolation/NearestNeighborTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static java.lang.Float.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class NearestNeighborTest {
@@ -16,10 +18,10 @@ public class NearestNeighborTest {
     public void knn_Should_Return_DataPoint(){
         // testing data
         ArrayList<DataPoint> neighbors = new ArrayList<>();
-        neighbors.add(new DataPoint(3, 3, 37, -4));
-        neighbors.add(new DataPoint(3, 4, 43, -4));
-        neighbors.add(new DataPoint(10, 9, 73, -5));
-        neighbors.add(new DataPoint(2, 3, 90, -3));
+        neighbors.add(new DataPoint(3, 3, 37, 4));
+        neighbors.add(new DataPoint(3, 4, 43, 4));
+        neighbors.add(new DataPoint(10, 9, 73, 5));
+        neighbors.add(new DataPoint(2, 3, 90, 30));
 
         // Creating point that we are interpolating
         DataPoint interpolatingPoint = new DataPoint(2, 3, NaN, -4);
@@ -32,7 +34,27 @@ public class NearestNeighborTest {
             interpolatingPoint.setTemperatureK((float)resultTemp);
             assertEquals(expectedResultDataPoint.getTemperatureK(), interpolatingPoint.getTemperatureK());
         }
-        catch (Exception e){ logger.warning("Error with interpolation" + e); }
+        catch (Exception e){ logger.warning("Error with knn_should_return_datapoint" + e); }
+
+    }
+    @Test
+    public void nearestNeighbor_test(){
+        ArrayList<DataPoint> dataPointArrayList = new ArrayList<>(Arrays.asList(
+                new DataPoint(100, -90, Float.NaN, 0),
+                new DataPoint(101, -90, 49, 22),
+                new DataPoint(103, -90, Float.NaN, 11),
+                new DataPoint(103, -89, Float.NaN, 40)
+        ));
+        DataPoint dataPointToInterpolate = new DataPoint(103 -89,40,11);
+        ArrayList<DataPoint> result = new ArrayList<>();
+        result = NearestNeighbor.getNearestNeighbor(dataPointArrayList,dataPointToInterpolate,2);
+
+        ArrayList<DataPoint> expectedResult = new ArrayList<>(Arrays.asList(
+                new DataPoint(103,-90, Float.NaN, 11),
+                new DataPoint(101, -90, 49, 22)
+        ));
+//        assertEquals(expectedResult.get(0), result.get(0));
+        System.out.println(result.get(0));
 
     }
 }
